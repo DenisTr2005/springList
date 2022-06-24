1b21f787b2a57a767e9039f40e60659c62f1a492
Денис <den_2005@list.ru>
2022-06-24 23:14:03 +0300

    master commit

---
+++ src/main/java/denistr/springlist/SpringListApplication.java
@@ -0,0 +1,13 @@
+package denistr.springlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringListApplication.class, args);
+    }
+
+}
